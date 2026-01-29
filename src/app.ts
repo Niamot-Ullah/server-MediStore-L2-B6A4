599b54f3bf2e66c2848d1b61cf30a166f50391b3
Niamot Ullah <niamotullah009@gmail.com>
2026-01-30 00:38:28 +0600

    user api has been created

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { medicineRouter } from './modules/medicine/medicine.router';
 import { categoriesRouter } from './modules/categories/categories.router';
 import { auth } from './lib/auth';
 import { reviewsRouter } from './modules/reviews/reviews.router';
+import { userRouter } from './modules/user/user.router';
 
 const app = express();
 
@@ -30,6 +31,7 @@ app.all("/api/auth/*splat", toNodeHandler(auth));
 app.use("/api/medicine", medicineRouter)
 app.use("/api/categories", categoriesRouter)
 app.use("/api/reviews", reviewsRouter)
+app.use("/api/users/me", userRouter)
 
 
 
